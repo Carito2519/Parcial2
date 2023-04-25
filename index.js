30ee0f69148fa9943dc90ddac8220db4e86c32da
carolina leal <carola251933@gmail.com>
2023-04-25 16:15:41 -0500

    Correcion INDEX

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 import express from 'express';
 import roomsRouter from './routes/rooms.routes.js';
 import bookingsRouter from './routes/bookings.routes.js';
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 const app = express();
 app.use(express.json());
 app.use('/rooms', roomsRouter);
 app.use('/bookings', bookingsRouter);
 
-app.listen(5000);
+app.listen(process.env.PORT);
+console.log(`Escuchando en puerto ${process.env.PORT}`);
 
 export default app;
